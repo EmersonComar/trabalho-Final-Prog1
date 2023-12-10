0f30821580aaea3aea95f8b7b5de3983573664d5
Emerson <emerson.comar@estudante.uffs.edu.br>
2023-12-10 18:05:56 -0300

    Documentação Baralho

--- src/Baralho.java
+++ src/Baralho.java
@@ -2,15 +2,29 @@ import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 
+/**
+ * A classe Baralho representa um baralho de cartas, contendo métodos para criar, embaralhar e distribuir cartas.
+ */
 public class Baralho {
+
+    /** A lista de cartas no baralho. */
     private List<Carta> cartas;
 
+    /**
+     * Construtor que inicializa o baralho, cria as cartas, e as embaralha.
+     */
     public Baralho() {
         this.cartas = new ArrayList<>();
         criarBaralho();
         embaralhar();
     }
 
+    /**
+     * Distribui uma quantidade específica de cartas do baralho.
+     * @param quantidade A quantidade de cartas a ser distribuída.
+     * @return Uma lista contendo as cartas distribuídas.
+     * @throws IllegalArgumentException Se a quantidade de cartas solicitadas for maior do que a disponível no baralho.
+     */
     public List<Carta> distribuirCartas(int quantidade) {
         if(quantidade > cartas.size()){
             throw new IllegalArgumentException("Quantidade de cartas insuficientes");
@@ -22,6 +36,11 @@ public class Baralho {
         return cartasDistribuidas;
     }
 
+    /**
+     * Distribui uma única carta do baralho.
+     * @return A carta distribuída.
+     * @throws IllegalArgumentException Se o baralho estiver vazio.
+     */
     public Carta distribuirUmaCarta(){
         if (cartas.isEmpty()){
             throw new IllegalArgumentException("O baralho está vazio");
@@ -32,12 +51,18 @@ public class Baralho {
 
     // Métodos auxiliares
 
+    /**
+     * Cria as 109 cartas no baralho.
+     */
     private void criarBaralho() {
         for (int i = 1; i <= 109; i++) {
             cartas.add(new Carta(i));
         }
     }
 
+    /**
+     * Embaralha as cartas no baralho.
+     */
     private void embaralhar() {
         Collections.shuffle(cartas);
     }
