8b49d9614efb2eed498e89de289baeff9c782f31
Emerson <emerson.comar@estudante.uffs.edu.br>
2023-12-10 18:16:05 -0300

    Documentação Tabuleiro

--- src/Tabuleiro.java
+++ src/Tabuleiro.java
@@ -1,19 +1,32 @@
 import java.util.ArrayList;
 import java.util.List;
 
+/**
+ * A classe Tabuleiro representa o tabuleiro de jogo com trilhas de cartas.
+ */
 public class Tabuleiro {
+
+    /** As linhas do tabuleiro, cada uma representando uma trilha de cartas. */
     private List<List<Carta>> linhas;
 
+    /**
+     * Construtor que inicializa o tabuleiro com trilhas de cartas a partir de um baralho.
+     * @param baralho O baralho a ser usado para inicializar as trilhas.
+     * @param qtdLinhas A quantidade de trilhas no tabuleiro.
+     */
     public Tabuleiro(Baralho baralho, int qtdLinhas) {
         this.linhas = new ArrayList<>();
         inicializarLinhas(baralho, qtdLinhas);
     }
 
+    /**
+     * Exibe o estado atual do tabuleiro, mostrando as cartas em cada trilha.
+     */
     public void exibirTabuleiro() {
         System.out.println("\t\t-------------Tabuleiro--------------");
-        for(int i=0; i<this.linhas.size(); i++){
-            System.out.printf("Linha %d\t\t|", i+1);
-            for(int j=0; j<this.linhas.get(i).size(); j++){
+        for(int i = 0; i < this.linhas.size(); i++){
+            System.out.printf("Linha %d\t\t|", i + 1);
+            for(int j = 0; j < this.linhas.get(i).size(); j++){
                 System.out.printf("%d\t", this.linhas.get(i).get(j).getNumero());
             }
             System.out.println();
@@ -21,6 +34,10 @@ public class Tabuleiro {
         System.out.printf("\t\t-----------------------------------\n");
     }
 
+    /**
+     * Atualiza o tabuleiro com base nas jogadas dos jogadores, movendo cartas para as trilhas apropriadas.
+     * @param jogadores A lista de jogadores participando do jogo.
+     */
     public void atualizaTabuleiro(List<Jogador> jogadores) {
         for (Jogador jogador : jogadores) {
             Carta jogada = jogador.getJogada();
@@ -29,7 +46,7 @@ public class Tabuleiro {
             int indexDaLinha = 0;
             int diferenca = 110;
 
-            for (int i=0; i<linhas.size(); i++){
+            for (int i = 0; i < linhas.size(); i++){
                 List<Carta> trilha = linhas.get(i);
                 Carta ultimaCartaDaTrilha = trilha.get(trilha.size() - 1);
 
@@ -68,18 +85,34 @@ public class Tabuleiro {
 
     // Métodos auxiliares
 
+    /**
+     * Calcula a diferença entre os números de duas cartas.
+     * @param jogada A carta jogada pelo jogador.
+     * @param ultimaCartaDaTrilha A última carta de uma trilha no tabuleiro.
+     * @return A diferença entre os números das cartas.
+     */
     private int calculaDiferenca(Carta jogada, Carta ultimaCartaDaTrilha){
         return jogada.getNumero() - ultimaCartaDaTrilha.getNumero();
     }
 
+    /**
+     * Inicializa as trilhas do tabuleiro, distribuindo uma carta de baralho para cada trilha.
+     * @param baralho O baralho usado para inicializar as trilhas.
+     * @param qtdLinhas A quantidade de trilhas no tabuleiro.
+     */
     private void inicializarLinhas(Baralho baralho, int qtdLinhas) {
-        for(int i=0; i<qtdLinhas; i++){
+        for(int i = 0; i < qtdLinhas; i++){
             List<Carta> trilha = new ArrayList<>();
             trilha.add(baralho.distribuirUmaCarta());
             linhas.add(trilha);
         }
     }
 
+    /**
+     * Soma os pontos das cartas em uma trilha.
+     * @param linha A trilha contendo as cartas.
+     * @return A pontuação total das cartas na trilha.
+     */
     private int somarPontuacao(List<Carta> linha){
         int pontos = 0;
         for(Carta carta: linha){
